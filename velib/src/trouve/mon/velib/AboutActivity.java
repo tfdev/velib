fd19552e9c020d7cdf915581c3db17462634ceb8
tfdev <tangdev@outlook.fr>
2014-08-14 23:33:56 +0200

    About activity - to finish

---
+++ velib/src/trouve/mon/velib/AboutActivity.java
@@ -0,0 +1,46 @@
+package trouve.mon.velib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends Activity {
+
+	private Button mailButton;
+	private Button shareButton;
+	private Button rateButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		setUpView();
+	}
+	
+	
+	private void setUpView(){
+		mailButton = (Button) findViewById(R.id.mail);
+		shareButton = (Button) findViewById(R.id.share);
+		rateButton = (Button) findViewById(R.id.rate);
+		
+		mailButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				
+			}
+		});
+		shareButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				
+			}
+		});
+		rateButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				
+			}
+		});
+	}
+}
